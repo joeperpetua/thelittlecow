dad9ed0bbfc130ec2fcdb89f3a2905e799722d56
joeperpetua <qwerty.perpetua@gmail.com>
2020-08-29 23:55:10 +0200

    Added modals with items list, have to fix total prices for each user on product add / remove

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ let userArray = [];
 let itemArray = [];
 let userList;
 let itemList;
+let total = 0;
+let totalText;
 window.onload = () => {
     user_input = document.getElementById('person');
     item_input = document.getElementById('item');
     price_input = document.getElementById('price');
     userList = document.getElementById('list-user');
     itemList = document.getElementById('list-item');
-
+    totalText = document.getElementById('total');
 
     user_input.addEventListener("keyup", (e) => {
         e.preventDefault();
@@ -29,9 +31,18 @@ window.onload = () => {
 }
 
 addPerson = () => {
-    userArray.push(user_input.value);
+    let temp = {name: user_input.value, items: [], price: 0};
+    if (itemArray.lenght != 0) {
+        itemArray.forEach(item => {
+            temp.items.push(item);
+            addItemToUsers(item, temp);
+        });
+    }
+    userArray.push(temp);
     user_input.value = "";
-    renderList();
+
+    updatePrice();
+    render();
 }
 
 addItem = () => {
@@ -39,29 +50,105 @@ addItem = () => {
     itemArray.push(temp);
     item_input.value = "";
     price_input.value = "";
-    renderList();
+    addItemToUsers(temp);
+    render();
 }
 
 removePerson = (index) => {
     userArray.splice(index, 1);
-    renderList();
+    updatePrice();
+    render();
 }
 
 removeItem = (index) => {
     itemArray.splice(index, 1);
-    renderList();
+    updatePrice();
+    render();
 }
 
-renderList = () => {
+updatePrice = () => {
+    total = 0;
+    userArray.forEach(user => {
+        user.price = 0;
+    });
+
+    itemArray.forEach((item, itemIndex) => {
+        total += parseFloat(item.price);
+
+        userArray.forEach((user, userIndex) => {
+            console.log(user.items[itemIndex].name, item.name);
+            if (user.items[itemIndex].name === item.name) {
+                user.price += parseFloat(item.price);
+            }
+        });
+    });
+
+    
+}
+
+addItemToUsers = (item, argsUser) => {
+    if (argsUser) {
+        argsUser.items.push(item);
+    }else{
+        userArray.forEach(user => {
+            user.items.push(item);
+        });
+    }
+    updatePrice();
+}
+
+render = () => {
     let tempHTML = "";
     for (let i = 0; i < userArray.length; i++) {
+        let listCheckBox = "";
+        let checked = "";
+        itemArray.forEach((item, itemIndex)=> {
+
+            userArray[i].items.forEach((userItem, userItemIndex) => {
+                if (userItemIndex === itemIndex) {
+                    checked = "checked";
+                }
+            });
+
+            listCheckBox += `
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="item-${itemIndex}-checkbox" ${checked}>
+                    <label class="custom-control-label" for="item-${itemIndex}-checkbox">${item.name}</label>
+                </div>
+            `;
+        });
         tempHTML += `
             <div class="input-group mb-3">
-                <input type="text" class="form-control" value="${userArray[i]}" id="user-${i}" disabled>
+                <input type="text" class="form-control" value="${userArray[i].name}" id="user-${i}" disabled>
                 <div class="input-group-append">
-                    <button class="btn btn-outline-secondary" type="button" id="user_btn" onclick="removePerson(${i})">Remove</button>
+                    <input type="text" class="form-control" value="${userArray[i].price}" id="user-price-${i}" disabled>
+                    <span class="input-group-text">€</span>
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-user-${i}">M</button>
+                    <button class="btn btn-outline-secondary" type="button" id="user_btn" onclick="removePerson(${i})">X</button>
+                    <!-- Modal -->
+                    <div class="modal fade" id="modal-user-${i}" tabindex="-1" role="dialog" aria-labelledby="modal-user-${i}" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title">${userArray[i].name}</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    ${listCheckBox}
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+
+
         `;
         //tempHTML += `<li class="list-group-item" id="user-${i}"><p class="list-text">${userArray[i]}</p> <button class="btn btn-danger float-right" onclick="removePerson(${i})">Remove</button></li>`;
     }
@@ -75,7 +162,7 @@ renderList = () => {
                 <div class="input-group-append">
                     <input type="text" class="form-control" value="${itemArray[i].price}" id="price-${i}" disabled>
                     <span class="input-group-text">€</span>
-                    <button class="btn btn-outline-secondary" type="button" id="item_btn" onclick="removeItem(${i})">Remove</button>
+                    <button class="btn btn-outline-secondary" type="button" id="item_btn" onclick="removeItem(${i})">X</button>
                 </div>
             </div>
         `;
@@ -83,6 +170,8 @@ renderList = () => {
     }
     itemList.innerHTML = tempHTML;
 
+    tempHTML = `<h1>Total: $${total}<h1>`;
+    totalText.innerHTML = tempHTML;
     console.log(userArray, itemArray)
 }
 
